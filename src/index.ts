b7effa0815dcdc0ffebca8f8c1b180c4978b9d86
Satyajeet Singh <satyajeet1152000@gmail.com>
2025-02-08 21:18:22 +0530

    build errror fix try[#5]

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import logger from './config/logger.js';
 import app from './app.js';
 import config from './config/config.js';
 
+let server: any;
+
 mongoose
     .connect(config.MONGODB_URI)
     .then(() => {
         logger.info('MongoDB connected successfully');
-        app.listen(config.PORT, () => {
+        server = app.listen(config.PORT, () => {
             logger.info(`Server is running on port ${config.PORT}: http://localhost:${config.PORT}`);
         });
     })
@@ -16,45 +18,30 @@ mongoose
         process.exit(1);
     });
 
-// let server: any;
+const exitHandler = () => {
+    if (server) {
+        server.close(() => {
+            logger.info('Server closed');
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
+};
 
-// mongoose
-//     .connect(config.MONGODB_URI)
-//     .then(() => {
-//         logger.info('MongoDB connected successfully');
-//         server = app.listen(config.PORT, () => {
-//             logger.info(`Server is running on port ${config.PORT}: http://localhost:${config.PORT}`);
-//         });
-//     })
-//     .catch((err) => {
-//         logger.error(err);
-//         process.exit(1);
-//     });
+const unexpectedErrorHandler = (err: string) => {
+    logger.error(err);
+    exitHandler();
+};
 
-// const exitHandler = () => {
-//     if (server) {
-//         server.close(() => {
-//             logger.info('Server closed');
-//             process.exit(1);
-//         });
-//     } else {
-//         process.exit(1);
-//     }
-// };
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
 
-// const unexpectedErrorHandler = (err: string) => {
-//     logger.error(err);
-//     exitHandler();
-// };
-
-// process.on('uncaughtException', unexpectedErrorHandler);
-// process.on('unhandledRejection', unexpectedErrorHandler);
-
-// process.on('SIGTERM', () => {
-//     logger.info('SIGTERM received');
-//     if (server) {
-//         server.close(() => {
-//             logger.info('Server closed');
-//         });
-//     }
-// });
+process.on('SIGTERM', () => {
+    logger.info('SIGTERM received');
+    if (server) {
+        server.close(() => {
+            logger.info('Server closed');
+        });
+    }
+});
